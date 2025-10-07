ffdfcba8ea86005f09a3b7a873d1d3b3e74f6669
Sascha Kehrli <skehrli@ethz.ch>
2025-10-07 10:18:46 +0200

    fix error in title

--- publish.el
+++ publish.el
@@ -188,7 +188,7 @@
                     ;; Empty string to cause a closing </script> tag
                     "")
             ,(when head-extra head-extra)
-            (title ,("Sascha Kehrli | Phd Student, MPI-SWS")))
+            (title "Sascha Kehrli | Phd Student, MPI-SWS"))
            (body ,@(unless exclude-header
                      (dw/site-header))
                  (div (@ (class "container"))
